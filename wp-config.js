76e03c51856d5ee0fee333280a2fa88d4885fe84
Justin Sitter <justin@jaytria.com>
2018-09-05 20:30:19 -0700

    clean config file

---
+++ wp-config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    "entry": "src/index.js",
+    "output": {
+        "path": __dirname+'/static',
+        "filename": "[name].[chunkhash:8].js"
+    },
+    "module": {
+        "rules": [
+            {
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader",
+                    "options": {
+                        "presets": [
+                            "env",
+                            "react"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
